dee56b19a100d57de2abdcb433885533ceb6db29
ToThePowerOfBeans <ignas.budzinskas@ktu.edu>
2025-04-10 21:18:09 +0300

    Added page to view entry details

---
+++ lib/widgets/entry_info.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:browser_extension/utils/Saver/saver.dart';
+import 'package:flutter/material.dart';
+
+class EntryPage extends StatefulWidget {
+  const EntryPage({super.key});
+
+  @override
+  State<EntryPage> createState() => _EntryPageState();
+}
+
+class _EntryPageState extends State<EntryPage> {
+  List<String>? _entries;
+  @override
+  void initState() {
+    super.initState();
+    _loadEntries();
+  }
+
+  void _loadEntries() {
+    _entries = Saver.readInfo();
+  }
+
+  List<Widget> _getEntryValues(int index) {
+    List<Widget> params = [];
+    Map<String, String> entry = jsonDecode(_entries![index]);
+    List<String> keys = entry.keys.toList();
+    for (var key in keys) {
+      if (key == 'favicon') continue;
+      if (entry[key] != '') {
+        params.add(Text("$key:\t${entry[key]}"));
+        params.add(SizedBox(height: 16));
+      }
+    }
+    return params;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int index = ModalRoute.of(context)!.settings.arguments as int;
+    return Scaffold(
+      appBar: AppBar(title: Text('Entry details')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: _getEntryValues(index)),
+      ),
+    );
+  }
+}
