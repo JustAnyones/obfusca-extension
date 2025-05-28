b022d19d076ad46cb9e86e3c291ed07466946b12
ToThePowerOfBeans <ignas.budzinskas@ktu.edu>
2025-05-28 15:41:26 +0300

    Can select and copy specific entry details

--- lib/widgets/entry_info.dart
+++ lib/widgets/entry_info.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:browser_extension/utils/Saver/saver.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class EntryPage extends StatefulWidget {
   const EntryPage({super.key});
@@ -22,11 +23,15 @@ class _EntryPageState extends State<EntryPage> {
     _entries = Saver.readInfo();
   }
 
-  List<Widget> _getEntryValues(int index) {
+  @override
+  Widget build(BuildContext context) {
+    int index = ModalRoute.of(context)!.settings.arguments as int;
+
     List<Widget> params = [];
     Map<String, String> entry = jsonDecode(_entries![index]);
     List<String> keys = entry.keys.toList();
     for (var key in keys) {
+      Widget row;
       if (key == 'favicon' || key == 'uid') continue;
       if (entry[key] != '' && entry[key] != null) {
         print(key);
@@ -35,21 +40,40 @@ class _EntryPageState extends State<EntryPage> {
           namespace = namespace.substring(11);
           namespace = namespace.replaceAll("_generator", "");
         }
-        params.add(Text("$namespace:\t${entry[key]}"));
+        namespace = namespace[0].toUpperCase() + namespace.substring(1);
+        row = Row(
+          children: [
+            Text(
+              "$namespace:",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 2),
+            SelectableText(entry[key]!),
+            SizedBox(width: 12),
+            IconButton(
+              onPressed: () async {
+                await Clipboard.setData(ClipboardData(text: entry[key]!));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(AppLocalizations.of(context)!.clipboard),
+                  ),
+                );
+              },
+              icon: Icon(Icons.content_copy),
+              alignment: Alignment.centerRight,
+            ),
+          ],
+        );
+        params.add(row);
         params.add(SizedBox(height: 16));
       }
     }
-    return params;
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    int index = ModalRoute.of(context)!.settings.arguments as int;
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.entry_details)),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(children: _getEntryValues(index)),
+        child: Column(children: params),
       ),
     );
   }
